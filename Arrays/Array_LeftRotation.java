a6fe2d7676a9a0a5cb7d41de3399555e079a6d09
chethankb007 <67217350+chethankb007@users.noreply.github.com>
2020-07-14 12:38:02 +0530

    Create Array_LeftRotation.java

---
+++ Arrays/Array_LeftRotation.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the rotLeft function below.
+    static int[] rotLeft(int[] a, int d) {
+        if(d>a.length)
+        d=d%10;
+        
+        int n = a.length;
+
+        reverse(a,0,d-1);
+        reverse(a,d,n-1);
+        reverse(a,0,n-1);
+
+        return a;
+    }
+    static void reverse(int[] arr,int low,int high)
+    {
+        while(low<high)
+        {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nd = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+
+        int d = Integer.parseInt(nd[1]);
+
+        int[] a = new int[n];
+
+        String[] aItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int aItem = Integer.parseInt(aItems[i]);
+            a[i] = aItem;
+        }
+
+        int[] result = rotLeft(a, d);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
